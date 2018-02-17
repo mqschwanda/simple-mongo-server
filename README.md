807f839d1565e48241f0bc06060e03acfaad39c3
mqschwanda <mqschwanda@gmail.com>
2018-02-17 00:05:11 -0500

    upload code

---
+++ README.md
@@ -0,0 +1,6 @@
+1. In a new terminal start up mongod
+  `mongod`
+2. In another new terminal open up a mongo shell
+  `mongo`
+3. In the same mongo shell create a new database
+  `use simple-mongo-server`
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true})) // handle json in response
+
+const MongoClient = require('mongodb').MongoClient
+let db
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('simple-mongo-server') // connect to database
+
+  if (db.collection('tasks').find().count() < 3) {
+    const tasks = [{label: 'example 1'}, {label: 'example 2'}, {label: 'example 3'}]
+    tasks.forEach(task => db.collection('tasks').insert(task))
+  }
+
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/tasks', function (req, res) {
+  db.collection('tasks').find().toArray(function (err, results) {
+    if (err) console.log(err)
+    console.log(results) // do whatever you want with the data on the server
+    res.json(results) // send the data back to the user
+  })
+})
